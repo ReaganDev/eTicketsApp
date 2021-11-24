f9cc5b0a169e122b88a6cf664d2c3a81a6810fea
Reagan Reuben <reagan.reuben@decagon.dev>
2021-11-24 02:35:15 +0100

    Created Cinema View

--- ETicketsApp/Models/Cinema.cs
+++ ETicketsApp/Models/Cinema.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ETicketsApp.Models
 {
     public class Cinema
     {
+        [Key]
         public int Id { get; set; }
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; }
         public List<Movie> Movies { get; set; }
     }
