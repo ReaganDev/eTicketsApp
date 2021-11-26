592280758579d018fbbb6458d1aace14ea4ba5dc
Reagan Reuben <reagan.reuben@decagon.dev>
2021-11-26 11:17:50 +0100

    Working with the movies data

---
+++ ETicketsApp/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETicketsApp.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
