bf3c47324fdbbeacf5d3e5d830e43cd3bce704a4
Reagan Reuben <reagan.reuben@decagon.dev>
2021-11-26 23:44:01 +0100

    shopping cart view and orders controller

---
+++ ETicketsApp/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using ETicketsApp.Data;
+using ETicketsApp.Data.Interfaces;
+using ETicketsApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicketsApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ShoppingCart _cart;
+        private readonly IMoviesService _service;
+
+        public OrdersController(ShoppingCart cart, IMoviesService service)
+        {
+            _cart = cart;
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            var items = _cart.GetItems();
+            _cart.ShoppingCartItems = items;
+            var res = new ShoppingCartVM()
+            {
+                ShoppingCart = _cart,
+                Total = _cart.GetShoppingCartTotal()
+            };
+            return View(res);
+        }
+    }
+}
---
+++ ETicketsApp/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ETicketsApp.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double Total { get; set; }
+    }
+}
